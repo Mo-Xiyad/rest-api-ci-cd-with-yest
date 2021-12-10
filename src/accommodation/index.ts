5bac57636f94689429a46df24b945a49cd76c74e
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-12-10 11:32:19 +0100

    enpoints created

--- src/accommodation/index.ts
+++ src/accommodation/index.ts
@@ -6,7 +6,11 @@ const accommodationRouter = express.Router();
 accommodationRouter.get("/", async (req, res, next) => {
   try {
     const accommodation = await AccommodationModel.find();
-    res.send(accommodation);
+    if (accommodation) {
+      res.status(200).send(accommodation);
+    } else {
+      res.status(404).send();
+    }
   } catch (error) {
     res.status(404).send();
     console.log();
@@ -14,24 +18,59 @@ accommodationRouter.get("/", async (req, res, next) => {
 });
 accommodationRouter.post("/", async (req, res, next) => {
   try {
+    const accommodation = new AccommodationModel(req.body);
+    await accommodation.save();
+    if (accommodation) {
+      res.status(201).send(accommodation._id);
+    } else {
+      res.status(400).send();
+    }
   } catch (error) {
+    res.status(400).send(); // this needs to change to next(httpCreateError())
     console.log(error);
   }
 });
 accommodationRouter.get("/:accId", async (req, res, next) => {
   try {
+    const id = req.params.accId;
+    const accommodation = await AccommodationModel.findById(id);
+    if (accommodation) {
+      res.status(200).send(accommodation);
+    } else {
+      res.status(404).send();
+    }
   } catch (error) {
+    res.status(400).send(); // this needs to change to next(httpCreateError())
     console.log(error);
   }
 });
 accommodationRouter.put("/:accId", async (req, res, next) => {
   try {
+    const id = req.params.accId;
+    const accommodation = await AccommodationModel.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    );
+    if (accommodation) {
+      res.status(200).send(accommodation);
+    } else {
+      res.status(400).send();
+    }
   } catch (error) {
+    res.status(400).send(); // this needs to change to next(httpCreateError())
     console.log(error);
   }
 });
 accommodationRouter.delete("/:accId", async (req, res, next) => {
   try {
+    const id = req.params.accId;
+    const accommodation = await AccommodationModel.findByIdAndDelete(id);
+    if (accommodation) {
+      res.status(204).send();
+    } else {
+      res.status(404).send();
+    }
   } catch (error) {
     console.log(error);
   }
